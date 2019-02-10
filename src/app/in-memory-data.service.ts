d88209341c1cdab19e9b2f3d35ec1f5a0dc67428
Erick F <erickflores785@gmail.com>
2019-02-09 21:38:00 -0600

    Last commit

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const heroes = [
+    const products = [
       { id: 11, name: 'Mr. Nice' },
       { id: 12, name: 'Narco' },
       { id: 13, name: 'Bombasto' },
@@ -19,7 +19,7 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' }
     ];
-    return {heroes};
+    return {products};
   }
 
   // Overrides the genId method to ensure that a hero always has an id.
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,26 +1,104 @@
 import { Injectable } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product } from './product';
-import { PRODUCTS } from './mock-products';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private productsUrl = 'api/products'; // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
-  getProducts(): Observable<Product[]> {
-    this.messageService.add('ProductService: fetched products');
-      return of(PRODUCTS);
+  /* GET products whose name contains search term */
+  searchProducts(term: string): Observable<Product[]> {
+    if (!term.trim()) {
+      // if not search term, return empty product array.
+      return of ([]);
+    }
+    return this.http.get<Product[]>(`${this.productsUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found products matching "${term}"`)),
+      catchError(this.handleError<Product[]>(`searchProducts`, []))
+    );
   }
 
+  /** GET products from the server */
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl).pipe(
+      tap(_ => this.log('fetched products')),
+      catchError(this.handleError('getProducts', []))
+    );
+  }
+
+  /** GET product by id. Will 404 if id not found */
   getProduct(id: number): Observable<Product> {
-    // TODO: send the message _after_ fetching the product
-    this.messageService.add(`ProductService: fetched product id=${id}`);
-    return of(PRODUCTS.find(product => product.id === id));
+    const url = `${this.productsUrl}/${id}`;
+    return this.http.get<Product>(url).pipe(
+      tap(_ => this.log(`fetched product id=${id}`)),
+      catchError(this.handleError<Product>(`getProduct id=${id}`))
+    );
+  }
+
+  ///// Save Methods ////
+
+  /** POST: add a new hero to the server */
+  addProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.productsUrl, product, httpOptions).pipe(
+      tap((newProduct: Product) => this.log(`added product w/ id=${newProduct.id}`)),
+      catchError(this.handleError<Product>(`addProduct`))
+    );
+  }
+
+  /** PUT: update the product on the server */
+  updateProduct(product: Product): Observable<any> {
+    return this.http.put(this.productsUrl, product, httpOptions).pipe(
+      tap(_ => this.log(`updated product id=${product.id}`)),
+      catchError(this.handleError<any>('updateProduct'))
+    );
+  }
+
+  /** DELETE: delete the product from the server  */
+  deleteProduct (product: Product | number): Observable<Product> {
+    const id = typeof product === 'number' ? product : product.id;
+    const url = `${this.productsUrl}/${id}`;
+
+    return this.http.delete<Product>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted product id=${id}`)),
+      catchError(this.handleError<Product>(`deleteHero`))
+    );
+    }
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  /** Log a ProductService message with the MessageService */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  private log(message: string) {
+    this.messageService.add(`ProductService: ${message}`);
   }
 }
